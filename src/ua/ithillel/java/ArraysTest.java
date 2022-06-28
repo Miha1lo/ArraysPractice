805798be58aa1fa99a830082084a3f4548ee5465
Miha1lo <mihailo.tyurin@gmail.com>
2022-06-28 19:14:45 +0300

    example #4, correct1

--- src/ua/ithillel/java/ArraysTest.java
+++ src/ua/ithillel/java/ArraysTest.java
@@ -5,7 +5,6 @@ public class ArraysTest {
 
         int[][] score = new int[3][4];
         int[] result = new int[score.length];
-        int sum;
 
         for (int i = 0; i < score.length; i++) {
             for (int j = 0; j < score[i].length; j++) {
@@ -18,11 +17,9 @@ public class ArraysTest {
         System.out.println();
 
         for (int i = 0; i < score.length; i++) {
-            sum = 0;
             for (int j = 0; j < score[i].length; j++) {
-                sum += score[i][j];
+                result[i] += score[i][j];
             }
-            result[i] = sum;
             System.out.println(result[i]);
         }
     }
